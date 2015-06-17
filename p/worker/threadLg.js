0f008bf77e289c2ab69940999cabcb1cd081d9cb
zhouyg <yunge@taovip.com>
2015-06-17 14:55:13 +0800

    load images by web worker

---
+++ p/worker/threadLg.js
@@ -0,0 +1,40 @@
+//加载的图片数量
+var imgMax = 0;
+var imgSrc = 'example.jpg';
+var back = 'back.php';
+
+var currentLoadedNum = 0;
+var base64CacheArr = [];
+
+var str = '0';
+
+var testlen = function(){
+    str = str+str;
+    postMessage(str);
+};
+
+var requestImage = function(i){
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET',back);
+    xhr.send();
+    xhr.onreadystatechange = function(){
+//        postMessage(xhr.readyState);
+        if(xhr.readyState === 4){
+            if(xhr.status === 200){
+                var imgBase64 = xhr.responseText;
+                base64CacheArr.push(imgBase64);
+                postMessage(base64CacheArr);
+            }
+        }else{
+        }
+    }
+};
+onmessage = function(evt){
+    var data = evt.data;
+    if(data.type === 'num'){
+        imgMax = data.max;
+        for(var i=0;i<imgMax;i++){
+            requestImage(i);
+        }
+    }
+}
