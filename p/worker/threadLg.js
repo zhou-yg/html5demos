054eed4d8e6b831785848e14d1592e0f4e4e48d8
zhouyg <yunge@taovip.com>
2015-06-23 14:51:45 +0800

    worker 没点卵用

--- p/worker/threadLg.js
+++ p/worker/threadLg.js
@@ -3,8 +3,11 @@ var imgMax = 0;
 var imgSrc = 'example.jpg';
 var back = 'back.php';
 
+var requestCompleteNum = 0;
 var currentLoadedNum = 0;
 var base64CacheArr = [];
+
+var startTime = endTime = costTime = 0;
 
 var str = '0';
 
@@ -23,6 +26,13 @@ var requestImage = function(i){
             if(xhr.status === 200){
                 var imgBase64 = xhr.responseText;
                 base64CacheArr.push(imgBase64);
+                requestCompleteNum++;
+
+                if(requestCompleteNum>=imgMax){
+                    endTime = +new Date();
+                    costTime = (endTime - startTime)/1000;
+                    postMessage(costTime);
+                }
             }
         }else{
         }
@@ -32,10 +42,11 @@ onmessage = function(evt){
     var data = evt.data;
     if(data.type === 'num'){
         imgMax = data.max;
+        startTime = +new Date();
         for(var i=0;i<imgMax;i++){
             requestImage(i);
         }
     }else if(data === 'resource'){
         postMessage(base64CacheArr);
     }
-}
+};
